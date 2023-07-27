ebf29ab3d14431cf8bb209e6aaeeec4b2df50cc4
p5sclub <p5sclub894@gmail.com>
2023-07-27 21:14:23 +0100

    first commit

--- JS/script.js
+++ JS/script.js
@@ -179,6 +179,7 @@ function loadCartItems () {
         "₦" + carTotal;
     }
     updateCartIcon();
+    clearCart();
 }
 
 // Quantity In Cart Icon
@@ -193,4 +194,11 @@ function updateCartIcon () {
     }
     var cartIcon = document.querySelector("#cart-icon");
     cartIcon.setAttribute("data-quantity", quantity);
+}
+// Clear Cart Item After Successful Payment
+function clearCart(){
+    var cartContent = document.getElementsByClassName("cart-content")[0];
+    cartContent.innerHTML = "";
+    updatetotal();
+    localStorage.removeItem("cartItems");
 }
